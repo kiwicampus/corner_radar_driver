3214fcf1e328f6ea52905be15c6926471ae56aeb
jhairssteven <71862429+jhairssteven@users.noreply.github.com>
2025-02-11 20:27:54 +0000

    [LAUNCH FILES] Update launch file to include dependencies

--- corner_radar_driver/launch/receiver_launch.py
+++ corner_radar_driver/launch/receiver_launch.py
@@ -13,8 +13,9 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
@@ -26,10 +27,36 @@ def generate_launch_description():
         'receiver_params.yaml'
     ])
 
+    # Include the socket_can_bridge launch file
+    socketcan_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource([
+            FindPackageShare('ros2_socketcan'),
+            '/launch/socket_can_bridge.launch.xml'
+        ]),
+        launch_arguments={
+            'interface': 'can0',
+            'enable_can_fd': 'true',
+            'sender_timeout_sec': '0.1',
+            'from_can_bus_topic': 'from_can_bus_fd',
+            'to_can_bus_topic': 'to_can_bus_fd'
+        }.items()
+    )
+
     return LaunchDescription([
         DeclareLaunchArgument('params',
-                              default_value=params,
-                              description='Parameters for receiver'),
+                            default_value=params,
+                            description='Parameters for receiver'),
+        
+        # Launch the CAN bridge
+        socketcan_launch,
+        
+        # Send initial CAN message
+        ExecuteProcess(
+            cmd=['cansend', 'can0', '401##10102030405060708'],
+            output='screen'
+        ),
+        
+        # Launch the receiver node
         Node(
             package='corner_radar_driver',
             executable='receiver',
